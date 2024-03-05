f99a2b97c64e01f37c2940183e0fc56f4f87da8a
Eljah Capers <elicasper2000@yahoo.com>
2024-03-05 15:27:46 -0500

    Initial push React Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from '@mui/material';
+import { theme } from "./theme";
+
+const App = () => {
+  return <ThemeProvider theme={}>
+
+  </ThemeProvider>;
+};
+
+export default App;
