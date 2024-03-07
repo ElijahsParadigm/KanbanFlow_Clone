92540dfdf8956a05e40a08d2d63466cc887bdb35
Eljah Capers <elicasper2000@yahoo.com>
2024-03-07 01:27:15 -0500

    Installed and Setup Firebase

---
+++ src/firebse.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2dTS2uFUdLyIU_8mB87guLGDHc8wwfsw",
+  authDomain: "flowboard-be872.firebaseapp.com",
+  projectId: "flowboard-be872",
+  storageBucket: "flowboard-be872.appspot.com",
+  messagingSenderId: "131657860441",
+  appId: "1:131657860441:web:41bae24dd6f8a213f37848",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const fbFunctions = getFunctions(app);
+
+if (process.env.NODE_ENV === "development") {
+  connectAuthEmulator(auth, "http://localhost:9098");
+  connectFirestoreEmulator(db, "localhost", 8082);
+  connectFunctionsEmulator(fbFunctions, "localhost", 5002);
+}
